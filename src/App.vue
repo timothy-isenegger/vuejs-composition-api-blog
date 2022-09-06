558e2bc71749db7eaaeaa159e500ddc486579095
Timothy <timmy.isenegger@gmail.com>
2022-09-06 11:11:11 +0200

    [TASK] First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="p-5 bg-green-400 text-white mb-10">
+    <router-link :to="{ name: 'Home' }">Snazzy Fake Blog</router-link>
+  </nav>
+  <div class="container">
+    <router-view></router-view>
+    <div v-if="isLoading">Loading...</div>
+  </div>
+</template>
+
+<script setup>
+import usePageRequests from './composables/usePageRequests';
+const { isLoading } = usePageRequests();
+</script>
+<style lang="css">
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-wrap flex-grow">
+    <PostCard v-for="post in posts" :key="post.id" :post="post" />
+  </div>
+</template>
+<script setup>
+import useResource from '../composables/useResource';
+import PostCard from '../components/PostCard.vue';
+const { items: posts, fetchAll } = useResource('posts');
+fetchAll();
+</script>
