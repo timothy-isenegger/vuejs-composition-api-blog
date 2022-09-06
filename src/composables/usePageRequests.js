4aa7b175177d90e1708290ac1587e04e9983f46c
Timothy <timmy.isenegger@gmail.com>
2022-09-06 11:11:17 +0200

    [TASK] Add composables

---
+++ src/composables/usePageRequests.js
@@ -0,0 +1,21 @@
+import { reactive, computed } from 'vue';
+const activeRequests = reactive([]);
+
+export default function usePageRequests() {
+  const isLoading = computed(() => !!activeRequests.length);
+
+  const makeRequest = async (url) => {
+    // push the url to the activeRequests
+    const index = activeRequests.length;
+    activeRequests[index] = url;
+
+    const response = await fetch(url).catch((error) => alert(error));
+    const data = await response.json();
+
+    // remove the url from activeRequests
+    activeRequests.splice(index, 1);
+
+    return data;
+  };
+  return { isLoading, makeRequest };
+}
